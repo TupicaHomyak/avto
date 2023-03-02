64fa3599d09a709da4faf2263e41c5f3672e1119
Olga Malkova <Vodeviiil@mail.ru>
2023-03-02 14:20:04 +0300

    константы сделаны.

--- src/GearBox.java
+++ src/GearBox.java
@@ -1,13 +1,13 @@
 public class GearBox {
     private int gear = 0;
-    private int maxGear = 6;
-    private int minGear = 1;
+    public static  final int MAX_GEAR = 6;
+    public static  final int MIN_GEAR = 1;
 
     public void shiftUp() {   //метод повышающий скорость
-       gear = gear < maxGear? gear + 1: gear;
+       gear = gear < MAX_GEAR ? gear + 1: gear;
     }
     public void shiftDown() { //метод понижающий скорость
-        gear = gear > minGear? gear - 1: gear;
+        gear = gear > MIN_GEAR ? gear - 1: gear;
     }
     public void switchRear() { //уход на заднюю скорость
         gear = gear > 1? gear: -1;
